b8032648ec2d27312070f43695fb3c29ae9d6c5c
Khyeji <khjji0502@gmail.com>
2022-08-19 16:14:20 +0900

    Feat: 커스텀 탭바 컨트롤러 세팅 및 홈탭에 관한 네비게이션바 아이템 구현

--- MyPetD/MyPetD/TabBar/MainTabBarViewController.swift
+++ MyPetD/MyPetD/TabBar/MainTabBarViewController.swift
@@ -11,7 +11,50 @@ class MainTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.updateNavigationItem(viewController: self.selectedViewController!)
+    }
+    
+    private func updateNavigationItem(viewController: UIViewController) {
+        switch viewController {
+        case is HomeViewController:
+            navigationController?.navigationBar.prefersLargeTitles = true
+            navigationItem.title = "LargeTitles"
+//            let image = UIImage(systemName: "sun.max")
+//            navigationController?.navigationBar.setBackgroundImage(image, for: .default)
+//            navigationController?.navigationBar.backgroundColor = .clear
+//            navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(systemName: "sun.max.fill")
+            
+            // customBarItem
+            let titleConfig = CustomBarItemConfiguration(title: "HomeView", action: { print("title tapped") })
+            let titleItem = UIBarButtonItem.generate(with: titleConfig)
+            navigationItem.leftBarButtonItem = titleItem
+            
+            let notificationConfig = CustomBarItemConfiguration(image: UIImage(systemName: "bell")) {
+                print("bell button tapped")
+            }
+            let notificationItem = UIBarButtonItem.generate(with: notificationConfig, width: 30)
+            let addConfig = CustomBarItemConfiguration(image: UIImage(systemName: "plus")) {
+                print("plus button tapped")
+            }
+            let addItem = UIBarButtonItem.generate(with: addConfig, width: 30)
+            navigationItem.rightBarButtonItems = [addItem, notificationItem]
+            
+            
+        default:
+            print("")
+        }
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension MainTabBarViewController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        updateNavigationItem(viewController: viewController)
     }
 }

3dce1553f28766e9cc8fbd66a41d512a1c78f907
Khyeji <khjji0502@gmail.com>
2022-08-19 10:50:10 +0900

    프로젝트 초기 세팅

---
+++ MyPetD/MyPetD/TabBar/MainTabBarViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabBarViewController.swift
+//  MyPetD
+//
+//  Created by heyji on 2022/08/19.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}

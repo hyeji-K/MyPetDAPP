ad1d82ec88f38e2eff51a6fe547109b922af6266
Khyeji <khjji0502@gmail.com>
2022-10-17 10:23:09 +0900

    Modi: 상품 모델 수정

--- MyPetD/MyPetD/Model/ProductInfo.swift
+++ MyPetD/MyPetD/Model/ProductInfo.swift
@@ -8,26 +8,35 @@
 import Foundation
 
 struct ProductInfo: Hashable, Identifiable, Codable {
-    let id: String
-    let imageOfProduct: String
-    let nameOfProduct: String
-    let expirationDate: String // Date 
-    let storedMethod: String
+    var id: String = UUID().uuidString
+    var image: String
+    var name: String
+    var expirationDate: String
+    var storedMethod: String
+    var memo: String
 //    let category: String
-    let memo: String
     
     var toDictionary: [String: Any] {
-        let dict: [String: Any] = ["id": id, "imageOfProduct": imageOfProduct, "nameOfProduct": nameOfProduct, "expirationDate": expirationDate, "storedMethod": storedMethod, "memo": memo]
+        let dict: [String: Any] = ["id": id, "image": image, "name": name, "expirationDate": expirationDate, "storedMethod": storedMethod, "memo": memo]
         return dict
+    }
+}
+
+extension Array where Element == ProductInfo {
+    func indexOfReminder(with id: ProductInfo.ID) -> Self.Index {
+        guard let index = firstIndex(where: { $0.id == id }) else {
+            fatalError()
+        }
+        return index
     }
 }
 
 extension ProductInfo {
     static let list = [
-        ProductInfo(id: "1", imageOfProduct: "Ellipse5", nameOfProduct: "차오츄르", expirationDate: "2022.01.02", storedMethod: "냉장고", memo: "우리 고양이가 짱 좋아하는 츄르!"),
-        ProductInfo(id: "2", imageOfProduct: "Ellipse4", nameOfProduct: "로얄캐닌 인도어", expirationDate: "2022.01.02", storedMethod: "실온", memo: "우리 고양이가 짱 좋아하는 사료!"),
-        ProductInfo(id: "3", imageOfProduct: "Ellipse3", nameOfProduct: "어쩌구 캔", expirationDate: "2022.01.02", storedMethod: "서랍", memo: "우리 고양이가 짱 좋아하는 캔!"),
-        ProductInfo(id: "4", imageOfProduct: "Ellipse2", nameOfProduct: "네모북어", expirationDate: "2022.01.02", storedMethod: "냉장고", memo: "우리 고양이가 짱 좋아하는 트릿!"),
-        ProductInfo(id: "5", imageOfProduct: "Ellipse1", nameOfProduct: "닭가슴살", expirationDate: "2022.01.02", storedMethod: "서랍", memo: "우리 고양이가 짱 좋아하는 간식!"),
+        ProductInfo(image: "Ellipse5", name: "차오츄르", expirationDate: "2022.01.02", storedMethod: "냉장고", memo: "우리 고양이가 짱 좋아하는 츄르!"),
+        ProductInfo(image: "Ellipse4", name: "로얄캐닌 인도어", expirationDate: "2022.01.02", storedMethod: "실온", memo: "우리 고양이가 짱 좋아하는 사료!"),
+        ProductInfo(image: "Ellipse3", name: "어쩌구 캔", expirationDate: "2022.01.02", storedMethod: "서랍", memo: "우리 고양이가 짱 좋아하는 캔!"),
+        ProductInfo(image: "Ellipse2", name: "네모북어", expirationDate: "2022.01.02", storedMethod: "냉장고", memo: "우리 고양이가 짱 좋아하는 트릿!"),
+        ProductInfo(image: "Ellipse1", name: "닭가슴살", expirationDate: "2022.01.02", storedMethod: "서랍", memo: "우리 고양이가 짱 좋아하는 간식!"),
     ]
 }
